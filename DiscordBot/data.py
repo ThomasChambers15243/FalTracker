815bba1c161d6a6e774f05f4dc7bed17ff23ad77
TomChambers <tc262389@falmouth.ac.uk>
2021-10-12 14:10:48 +0100

    Added comments and data fields

--- DiscordBot/data.py
+++ DiscordBot/data.py
@@ -1,8 +1,10 @@
 data = {
     "botInfo" : {
+        "Version" : "v1.00.00",
         "Name" : "FalTracker",
         "Description" : "Bot to help Falmouth Uni Students find out quick information about uni services",
-        "Author" : "Tom Chambers, @OffDutySquirrel#3467"
+        "Author" : "Tom Chambers, @OffDutySquirrel#3467",
+        "Python Version" : "Python 3.10.0"
     },
     "keys" : {
         "token" : "ODk3MTk4MDg3NDE0NjIwMTcy.YWSK1Q.1JUA2_tjXY2-bPp7yGT0bsK29sg"
