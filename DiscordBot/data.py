1281379a638d7186afd6947053cd6dc3a44557c4
TomChambers <tc262389@falmouth.ac.uk>
2021-10-12 14:21:29 +0100

    Added keys to data.py

--- DiscordBot/data.py
+++ DiscordBot/data.py
@@ -7,6 +7,9 @@ data = {
         "Python Version" : "Python 3.10.0"
     },
     "keys" : {
-        "token" : "ODk3MTk4MDg3NDE0NjIwMTcy.YWSK1Q.1JUA2_tjXY2-bPp7yGT0bsK29sg"
+        "token" : "ODk3MTk4MDg3NDE0NjIwMTcy.YWSK1Q.1JUA2_tjXY2-bPp7yGT0bsK29sg",
+        "Applicatoin ID" : "897198087414620172",
+        "Public Key" : "930a7810e8a7c75155438cf0d54276b9de7d8cc8bf32263c11ee2363dba65fe1",
+        "Client ID" : "897198087414620172"
     }
 }
