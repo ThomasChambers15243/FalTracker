4129cc60b7db04083270cec2b3a0758bfe82d933
TomChambers <tc262389@falmouth.ac.uk>
2022-08-19 14:27:53 +0100

    Moved files out of module

---
+++ data.py
@@ -0,0 +1,53 @@
+botData = {
+    "botInfo" : {
+        "Version" : "v1.00.00",
+        "Name" : "FalTracker",
+        "Description" : "Bot to help Falmouth Uni Students find out quick information about uni services",
+        "Author" : "Tom Chambers, @OffDutySquirrel#3467",
+        "Python Version" : "Python 3.10.0"
+    },
+    "keys" : {
+        "token" : "ODk3MTk4MDg3NDE0NjIwMTcy.GDbPFL.z0vXvKQ0isR1dLk005znVTQdjw3FArFZMtyReA",
+        "Applicatoin ID" : "897198087414620172",
+        "Public Key" : "930a7810e8a7c75155438cf0d54276b9de7d8cc8bf32263c11ee2363dba65fe1",
+        "Client ID" : "897198087414620172"
+    },
+}
+data = {
+    "botInfo" : {
+            "Version" : "v1.00.00",
+            "Name" : "FalTracker",
+            "Description" : "Bot to help Falmouth Uni Students find out quick information about uni services",
+            "Author" : "Tom Chambers, @OffDutySquirrel#3467",
+            "Python Version" : "Python 3.10.0"
+    },
+    "keys" : {
+            "token" : "ODk3MTk4MDg3NDE0NjIwMTcy.GDbPFL.z0vXvKQ0isR1dLk005znVTQdjw3FArFZMtyReA",
+            "Applicatoin ID" : "897198087414620172",
+            "Public Key" : "930a7810e8a7c75155438cf0d54276b9de7d8cc8bf32263c11ee2363dba65fe1",
+            "Client ID" : "897198087414620172"
+    },
+    "webInfo" : {
+            "falmouthServiceURL" : "https://fxplus.ac.uk/service-status/",
+            "tableCol" : "tablepress-2"
+    },
+    "Util" : {
+        "TargetOpen" : "currently open",
+        "TargetClosed" : "currently closed"
+    },
+    "FoodAndDrink" : {
+        "Koofi": "https://fxplus.ac.uk/food-drink/koofi/",
+        "Amata" : "https://fxplus.ac.uk/food-drink/amata-cafe/",
+        "ESI" : "https://fxplus.ac.uk/food-drink/esi-cafe/",
+        "Stannary Bar": "https://fxplus.ac.uk/food-drink/stannary-bar/",
+        "Stannary Kitchen": "https://fxplus.ac.uk/food-drink/stannary-kitchen/",
+        "Fox": "https://fxplus.ac.uk/food-drink/fox-cafe/",
+        "Sus cafe": "https://fxplus.ac.uk/food-drink/the-sustainability-cafe/",
+        "Penryn Shop" : "https://fxplus.ac.uk/food-drink/penryn-campus-shop/",
+
+    },
+    "Facilities and Services" : {
+        "Falmouth Art Shop" : "https://fxplus.ac.uk/facilities-services/falmouth-campus-art-shop/",
+        "Flexsi Sports Centre" : "https://fxplus.ac.uk/facilities-services/flexsi-sports-centre/"
+    },
+}
