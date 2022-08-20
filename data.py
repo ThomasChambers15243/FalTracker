bcee6b2465a230ab924141c4bd53bfc0265d7f6d
TomChambers <tc262389@falmouth.ac.uk>
2022-08-20 12:34:24 +0100

    Removed token from data

--- data.py
+++ data.py
@@ -1,3 +1,4 @@
+
 botData = {
     "botInfo" : {
         "Version" : "v1.00.00",
@@ -7,26 +8,13 @@ botData = {
         "Python Version" : "Python 3.10.0"
     },
     "keys" : {
-        "token" : "ODk3MTk4MDg3NDE0NjIwMTcy.GDbPFL.z0vXvKQ0isR1dLk005znVTQdjw3FArFZMtyReA",
+        "token" : "O",
         "Applicatoin ID" : "897198087414620172",
         "Public Key" : "930a7810e8a7c75155438cf0d54276b9de7d8cc8bf32263c11ee2363dba65fe1",
         "Client ID" : "897198087414620172"
     },
 }
 data = {
-    "botInfo" : {
-            "Version" : "v1.00.00",
-            "Name" : "FalTracker",
-            "Description" : "Bot to help Falmouth Uni Students find out quick information about uni services",
-            "Author" : "Tom Chambers, @OffDutySquirrel#3467",
-            "Python Version" : "Python 3.10.0"
-    },
-    "keys" : {
-            "token" : "ODk3MTk4MDg3NDE0NjIwMTcy.GDbPFL.z0vXvKQ0isR1dLk005znVTQdjw3FArFZMtyReA",
-            "Applicatoin ID" : "897198087414620172",
-            "Public Key" : "930a7810e8a7c75155438cf0d54276b9de7d8cc8bf32263c11ee2363dba65fe1",
-            "Client ID" : "897198087414620172"
-    },
     "webInfo" : {
             "falmouthServiceURL" : "https://fxplus.ac.uk/service-status/",
             "tableCol" : "tablepress-2"
